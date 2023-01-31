75abec2b03048c70a9f43a624d9efb90b9f7c1af
thuthi <whquddn55@gmail.com>
2023-01-31 17:43:13 +0900

    feat: 5.4장 controller에서 어노테이션 기반으로 유저 정보 가져오기

---
+++ src/main/java/com/thuthi/springboot/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.thuthi.springboot.config;
+
+import com.thuthi.springboot.config.auth.LoginUserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
--- src/main/java/com/thuthi/springboot/web/IndexController.java
+++ src/main/java/com/thuthi/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.thuthi.springboot.web;
 
+import com.thuthi.springboot.config.auth.LoginUser;
 import com.thuthi.springboot.config.auth.dto.SessionUser;
 import com.thuthi.springboot.service.posts.PostsService;
 import com.thuthi.springboot.web.dto.PostsResponseDto;
@@ -17,10 +18,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(@LoginUser SessionUser user, Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
